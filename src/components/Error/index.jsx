54eb434a0267e47cdeb08b3537ca1d577340d4a7
Mikafrdev <mikadevfr@gmail.com>
2023-04-09 21:41:41 +0200

    WIP Page 404

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,41 +1,36 @@
-import styled from 'styled-components'
-import colors from '../../utils/style/colors'
-import { Link } from 'react-router-dom'
-import logo from '../../assets/logo.svg'
+import styled from 'styled-components';
+import { device } from '../../utils/style/Devices';
 
-const HomeLogo = styled.img`
-  height: 70px;
+const ErrorContainer = styled.section`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: center;
+    margin: 10px 0;
+    border-radius: 25px;
+    background-color: #FFF;
+
+    @media ${device.tablet} {
+        padding: 6px 0 20px 0;
+    }
+    
+    & h1 {
+
+    }
+
+    & p {
+      
+    }
+
 `
 
-const ErrorWrapper = styled.div`
-  margin: 30px;
-  display: flex;
-  flex-direction: column;
-  background-color: ${colors.backgroundFooter};
-  align-items: center;
-`
+export default function Error() {
 
-const ErrorTitle = styled.h1`
-  font-weight: 300;
-`
-
-const ErrorSubtitle = styled.h2`
-  font-weight: 300;
-  color: ${colors.backgroundFooter};
-`
-
-function Error() {
-  return (
-    <ErrorWrapper>
-      <ErrorTitle>404</ErrorTitle>
-      <ErrorSubtitle>
-        Oups! La page que vous demandez n'existe pas.
-      </ErrorSubtitle>
-      <Link to="/">
-        <HomeLogo src={logo} />
-      </Link>
-    </ErrorWrapper>
-  )
+    return (
+        <ErrorContainer>
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <link>Retourner sur la page d’accueil</link>
+        </ErrorContainer>
+    );
 }
-
-export default Error
